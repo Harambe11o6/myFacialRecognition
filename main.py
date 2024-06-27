879283d3f7767fdd5c93c9d7f1313ad38f605691
GREG <charanakash1106@gmail.com>
2024-06-27 12:10:48 +0530

    INITIAL COMMIT

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from tkinter import messagebox
 import loginInterface as li
 import shutil
 import time
-import threading
 from datetime import datetime
 import csv
 import mysql.connector
-def face_confidence(face_distance,face_match_threshold=0.6):
+def face_confidence(face_distance,face_match_threshold=0.6):#checking for face tolerance and approximating matching percentage
     range=(1.0-face_match_threshold)
     linear_val=(1.0-face_distance)/(range*2.0)
     if face_distance>face_match_threshold:
@@ -20,13 +19,13 @@ def face_confidence(face_distance,face_match_threshold=0.6):
     else:
         value=(linear_val+((1.0-linear_val)*math.pow((linear_val-0.5)*2,0.2)))*100
         return str(round(value,2))+'%'
-def show_message_box():
+def show_message_box():#function to show login successful
     dummy=li.uname
     messagebox.showinfo("Login Status", "Login Successful\n"+ dummy.upper()+ " of SPINTEG\n"+"DATE:"+ current_date+"\n"+"TIME:"+current_timer)
 
 
 
-class FaceRecognition:
+class FaceRecognition:#attributes of the face recognition
     face_locations=[]
     face_encodings=[]
     face_names=[]
@@ -35,21 +34,21 @@ class FaceRecognition:
     process_current_frame=True
     def __init__(self):
         self.encode_faces()
-    def encode_faces(self):
+    def encode_faces(self):#fetches the image from known people folder which has the face to be rcognised by the program
         for image in os.listdir('known_people'):
             face_image=face_recognition.load_image_file(f'known_people/{image}')
             face_encoding=face_recognition.face_encodings(face_image)[0]
             self.known_face_encoding.append(face_encoding)
             self.known_face_names.append(image)
         print(self.known_face_names)
-    def run_recognition(self):
+    def run_recognition(self):#starting the face recognition
 
-        video_capture=cv2.VideoCapture(0)
+        video_capture=cv2.VideoCapture(0)#the argument for this funtion depend on which camera you to use ,for default use 0. For external use 1,2,etc
         if not video_capture.isOpened():
             sys.exit('video source not found...')
         start = time.time()
-        while time.time() - start < 120:
-            ret,frame=video_capture.read()
+        while time.time() - start < 120:#running the loop for 120 second(2 minutes) because the login needs to expire if no match is found
+            ret,frame=video_capture.read()#starting to recognise the face in camera
             if self.process_current_frame:
                 small_frame=cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
                 rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
@@ -68,17 +67,17 @@ class FaceRecognition:
                         name=self.known_face_names[best_match_index]
                         confidence=face_confidence(face_distances[best_match_index])
                         self.face_names.append(f'{name}({confidence})')
-                        video_capture.release()
-                        employees = self.known_face_names.copy()
+                        video_capture.release()#stop the camera reading after match is found
+                        employees = self.known_face_names.copy()#creating a copy of known face _face names
                         self.face_names.append(name)
                         if name in self.known_face_names:
                             if name in employees:
                                 employees.remove(name)
                                 print(employees)
                                 current_time = now.strftime("%H-%M-%S")
-                                lnwriter.writerow([name, current_time])
+                                lnwriter.writerow([name, current_time])#writing the name of the employee and time he logged in
                         folder = 'known_people'
-                        for filename in os.listdir(folder):
+                        for filename in os.listdir(folder):#deleting employee face form the known_people folder after the matching is complete
                             file_path = os.path.join(folder, filename)
                             try:
                                 if os.path.isfile(file_path) or os.path.islink(file_path):
@@ -87,12 +86,12 @@ class FaceRecognition:
                                     shutil.rmtree(file_path)
                             except Exception as e:
                                 print('Failed to delete %s. Reason: %s' % (file_path, e))
-                        cv2.destroyAllWindows()
+                        cv2.destroyAllWindows()#closing the face recognition window
                         show_message_box()
                         break
 
 
-            self.process_current_frame=not self.process_current_frame
+            self.process_current_frame=not self.process_current_frame#creating a frame for the face recognised
             for(top,right,bottom,left),name in zip(self.face_locations,self.face_names):
                 top*=4
                 right*=4
@@ -104,14 +103,14 @@ class FaceRecognition:
                 cv2.putText(frame,name,(left+6,bottom-6),cv2.FONT_HERSHEY_DUPLEX,0.8,(255,255,255),1)
             if ret:
                 cv2.imshow('face Recognition',frame)
-                if cv2.waitKey(1) == ord('q'):
+                if cv2.waitKey(1) == ord('q'):#to force quit the program
                     break
             else:
                 break
-        else:
+        else:#over the time of 2 minutes
             messagebox.showinfo("Login Status", "Login expired ")
             folder = 'known_people'
-            for filename in os.listdir(folder):
+            for filename in os.listdir(folder):#deleting the image from known people
                 file_path = os.path.join(folder, filename)
                 try:
                     if os.path.isfile(file_path) or os.path.islink(file_path):
@@ -125,12 +124,12 @@ if __name__=='__main__':
 
     fr = FaceRecognition()
     now = datetime.now()
-    current_date = now.strftime("%Y-%m-%d")
-    current_timer=now.strftime("%H-%M-%S")
-    f = open(current_date + '.csv', 'a+', newline='')
+    current_date = now.strftime("%Y-%m-%d")#getting present date
+    current_timer=now.strftime("%H-%M-%S")#getting current timer
+    f = open(current_date + '.csv', 'a+', newline='')#opening the file object
     lnwriter=csv.writer(f)
     fr.run_recognition()
-    f.close()
+    f.close()#closing the file object
 
 
 
