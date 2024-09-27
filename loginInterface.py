1372cfb9e40ac7925081ca3b1ce439d6c60a89d2
GREG <charanakash1106@gmail.com>
2024-09-27 11:49:48 +0530

    GUI IMPROVED

--- loginInterface.py
+++ loginInterface.py
@@ -131,16 +131,16 @@ class LoginPage:
         self.login = Button(self.lgn_button_label,command=login, text='LOGIN', font=("yu gothic ui", 13, "bold"), width=25, bd=0,
                             bg='#3047ff', cursor='hand2', activebackground='#3047ff', fg='white')
         self.login.place(x=20, y=10)
-        """
+
         # ========================================================================
         # ============================Forgot password=============================
         # ========================================================================
-        self.forgot_button = Button(self.lgn_frame, text="Forgot Password ?",
+        self.forgot_button = Button(self.lgn_frame, text="Forgot Password ?",command=ForgotPasswd,
                                     font=("yu gothic ui", 13, "bold underline"), fg="white", relief=FLAT,
                                     activebackground="#040405"
                                     , borderwidth=0, background="#040405", cursor="hand2")
         self.forgot_button.place(x=630, y=510)
-        """
+
         # ========================================================================
         # ============================password====================================
         # ========================================================================
@@ -185,6 +185,8 @@ class LoginPage:
                                   , borderwidth=0, background="white", cursor="hand2")
         self.show_button.place(x=860, y=420)
         self.password_entry.config(show='*')
+
+
 class RegPage:
     def __init__(self, window):
         self.window = window
@@ -372,6 +374,109 @@ class RegPage:
         self.show_button.place(x=860, y=420)
         self.password_entry.config(show='*')
 
+class ForgotPasswordPage:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry('1166x718')
+        self.window.resizable(0, 0)
+        self.window.state('zoomed')
+        self.window.title('Login Page')
+
+        # ========================================================================
+        # ============================background image============================
+        # ========================================================================
+        self.bg_frame = Image.open('images\\background1.png')
+        photo = ImageTk.PhotoImage(self.bg_frame)
+        self.bg_panel = Label(self.window, image=photo)
+        self.bg_panel.image = photo
+        self.bg_panel.pack(fill='both', expand='yes')
+        # ====== Login Frame =========================
+        self.lgn_frame = Frame(self.window, bg='#040405', width=950, height=600)
+        self.lgn_frame.place(x=200, y=70)
+
+        # ========================================================================
+        # ========================================================
+        # ========================================================================
+        self.txt = "WELCOME"
+        self.heading = Label(self.lgn_frame, text=self.txt, font=('yu gothic ui', 25, "bold"), bg="#040405",
+                             fg='white',
+                             bd=5,
+                             relief=FLAT)
+        self.heading.place(x=80, y=30, width=300, height=30)
+
+        # ========================================================================
+        # ============ Left Side Image ================================================
+        # ========================================================================
+        self.side_image = Image.open('images\\vector.png')
+        photo = ImageTk.PhotoImage(self.side_image)
+        self.side_image_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.side_image_label.image = photo
+        self.side_image_label.place(x=5, y=100)
+
+        # ========================================================================
+        # ============ Sign In Image =============================================
+        # ========================================================================
+        self.sign_in_image = Image.open('images\\hyy.png')
+        photo = ImageTk.PhotoImage(self.sign_in_image)
+        self.sign_in_image_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.sign_in_image_label.image = photo
+        self.sign_in_image_label.place(x=620, y=130)
+        # ========================================================================
+        # ============================username====================================
+        # ========================================================================
+        self.username_label = Label(self.lgn_frame, text="Registered Username", bg="#040405", fg="#4f4e4d",
+                                    font=("yu gothic ui", 13, "bold"))
+        self.username_label.place(x=550, y=300)
+
+        self.username_entry = Entry(self.lgn_frame, textvariable=check_name, highlightthickness=0, relief=FLAT,
+                                    bg="#040405", fg="#6b6a69",
+                                    font=("yu gothic ui ", 12, "bold"), insertbackground='#6b6a69')
+        self.username_entry.place(x=580, y=335, width=270)
+
+        self.username_line = Canvas(self.lgn_frame, width=300, height=2.0, bg="#bdb9b1", highlightthickness=0)
+        self.username_line.place(x=550, y=359)
+        # ===== Username icon =========
+        self.username_icon = Image.open('images\\username_icon.png')
+        photo = ImageTk.PhotoImage(self.username_icon)
+        self.username_icon_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.username_icon_label.image = photo
+        self.username_icon_label.place(x=550, y=332)
+
+        # ========================================================================
+        # ============================login button================================
+        # ========================================================================
+        self.lgn_button = Image.open('images\\btn1.png')
+        photo = ImageTk.PhotoImage(self.lgn_button)
+        self.lgn_button_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.lgn_button_label.image = photo
+        self.lgn_button_label.place(x=550, y=450)
+        self.login = Button(self.lgn_button_label, command=login, text='SAVE NEW PASSWORD', font=("yu gothic ui", 13, "bold"),
+                            width=25, bd=0,
+                            bg='#3047ff', cursor='hand2', activebackground='#3047ff', fg='white')
+        self.login.place(x=20, y=10)
+        # ========================================================================
+        # ============================New password====================================
+        # ========================================================================
+        self.password_label = Label(self.lgn_frame, text="New Password", bg="#040405", fg="#4f4e4d",
+                                    font=("yu gothic ui", 13, "bold"))
+        self.password_label.place(x=550, y=380)
+
+        self.password_entry = Entry(self.lgn_frame, highlightthickness=0, textvariable=corrected_password, relief=FLAT,
+                                    bg="#040405", fg="#6b6a69",
+                                    font=("yu gothic ui", 12, "bold"), show="*", insertbackground='#6b6a69')
+        self.password_entry.place(x=580, y=416, width=244)
+
+        self.password_line = Canvas(self.lgn_frame, width=300, height=2.0, bg="#bdb9b1", highlightthickness=0)
+        self.password_line.place(x=550, y=440)
+        # ======== Password icon ================
+        self.password_icon = Image.open('images\\password_icon.png')
+        photo = ImageTk.PhotoImage(self.password_icon)
+        self.password_icon_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.password_icon_label.image = photo
+        self.password_icon_label.place(x=550, y=414)
+
+
+
 
 
 def regwrite():#validating registration details given by the user
@@ -397,6 +502,27 @@ def regwrite():#validating registration details given by the user
     cursor1.execute("insert into details values('{}','{}','{}','{}')".format(newuname,newpword,newemail,newfile))
     seccon.commit()
     seccon.close()
+def ForgotPasswd():
+    global forgotpassword_window
+    forgotpassword_window = Tk()
+    global check_name
+    global corrected_password
+    check_name=StringVar()
+    corrected_password=StringVar()
+    ForgotPasswordPage(forgotpassword_window)
+    ForgotPasswordPage.mainloop()
+def correction():
+    mycon=mysql.connector.connect(host="localhost ", user="root", passwd="SPINteg@123", database="sample")
+    cursor2=mycon.cursor()
+    cursor2.execute("select name from details")
+    names_list=cursor2.fetchall()
+    if(check_name not in names_list):
+        messagebox.showinfo("Login Status", "Username doesnt match with the database")
+    else:
+        cursor2.execute("update detail set DOB='{}' where name='{}'".format(corrected_password,check_name))
+    mycon.commit()
+    mycon.close()
+
 def Register():#creating a gui using tkinter
     global reg_window
     reg_window = Tk()
